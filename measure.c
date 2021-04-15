073c076f6498522f76f17708c617507d1bf44cdd
alebossi96 <alebossi96@hotmail.it>
2021-04-15 10:10:20 +0200

    FUNZIONA

--- measure.c
+++ measure.c
@@ -1,81 +1,91 @@
 #include"measure.h"
 #define SIZE_SEND 65
-
+#define WIZARD_OR_COMMAND false
 void initDMD(struct DMD *dmd){
 	hid_init();
 	dmd->handle = hid_open(0x0451, 0xc900, NULL);
 
 /*	START setup data needed*/
 	int RasterOrHadamard, nBasis, nMeas, startPositionPercentage, exp,dark_time, repeat,compress,sizeBatch;
-	printf("if Raster scan press 0, if Hadamard pattern press 1, if all mirrors press 2\n");
-	scanf("%d", &RasterOrHadamard);
-	do{
-		if(RasterOrHadamard == 1 || RasterOrHadamard== 0){
-			if(RasterOrHadamard == 0){
-				printf("\n\n RASTER \n\n");
-				printf("select dimension of lines: ");
-				scanf("%d",&nBasis);
-				printf("select starting position from 0 to 100: ");
-				scanf("%d",&startPositionPercentage);
+	
+	if(WIZARD_OR_COMMAND){
+		printf("if Raster scan press 0, if Hadamard pattern press 1, if all mirrors press 2\n");
+		scanf("%d", &RasterOrHadamard);
+		do{
+			if(RasterOrHadamard == 1 || RasterOrHadamard== 0){
+				if(RasterOrHadamard == 0){
+					printf("\n\n RASTER \n\n");
+					printf("select dimension of lines: ");
+					scanf("%d",&nBasis);
+					printf("select starting position from 0 to 100: ");
+					scanf("%d",&startPositionPercentage);
+				}
+				else{
+					printf("\n\n HADAMARD \n\n");
+					printf("select number of bases 	MUST BE POWER OF 2: ");
+					scanf("%d",&nBasis);
+					startPositionPercentage = 0;
+					//TODO mettere check su nBasis
+				}
+				printf("select number of measurement: ");
+				scanf("%d",&nMeas);
+				printf("what should the size batch be? -must be multiple of 24. probably: " );
+				scanf("%d",&sizeBatch);
+				
+				
+				printf("select exposure time in us: ");
+				scanf("%d", &exp);
+				printf("do you want to be repeated? if so press 1: ");
+				scanf("%d", &repeat);
+				if(repeat !=1) repeat = 0;
+				printf("do you want to be compressed? if so press 1: ");
+				scanf("%d", &compress);
+				if(compress !=1) compress = 0;
+						
+			}
+			else if(RasterOrHadamard == 2 ){
+				printf("ALL ONES\n");
+				nBasis =24; 
+				nMeas = 24; 
+
+
+				startPositionPercentage= 0;	
+				
+				
+				exp = 500000;    
+
+				repeat = 1;
+				compress = 0;
+
 			}
 			else{
-				printf("\n\n HADAMARD \n\n");
-				printf("select number of bases 	MUST BE POWER OF 2: ");
-				scanf("%d",&nBasis);
-				startPositionPercentage = 0;
-				//TODO mettere check su nBasis
+
+				nBasis =24; 
+				nMeas = 24; 
+
+
+				startPositionPercentage= 0;	
+				
+				
+				exp = 500000;    
+				
+
+				repeat = 1;
+				compress = 0;
+
+
 			}
-			printf("select number of measurement: ");
-			scanf("%d",&nMeas);
-			printf("what should the size batch be? -must be multiple of 24. probably: " );
-			scanf("%d",&sizeBatch);
-			
-			
-			printf("select exposure time in us: ");
-			scanf("%d", &exp);
-			printf("do you want to be repeated? if so press 1: ");
-			scanf("%d", &repeat);
-			if(repeat !=1) repeat = 0;
-			printf("do you want to be compressed? if so press 1: ");
-			scanf("%d", &compress);
-			if(compress !=1) compress = 0;
-					
-		}
-		else if(RasterOrHadamard == 2 ){
-			printf("ALL ONES\n");
-			nBasis =24; 
-			nMeas = 24; 
-
-
-			startPositionPercentage= 0;	
-			
-			
-			exp = 500000;    
-
-			repeat = 1;
-			compress = 0;
-
-		}
-		else{
-
-			nBasis =24; 
-			nMeas = 24; 
-
-
-			startPositionPercentage= 0;	
-			
-			
-			exp = 500000;    
-			
-
-			repeat = 1;
-			compress = 0;
-
-
-		}
-	}while(!(RasterOrHadamard == 0 || RasterOrHadamard == 1  || RasterOrHadamard == 2) );
-	
-		dark_time = 0; 
+		}while(!(RasterOrHadamard == 0 || RasterOrHadamard == 1  || RasterOrHadamard == 2) );
+	}
+	else{
+		nBasis =24; 
+		nMeas = 24; 
+		startPositionPercentage= 0;	
+		exp = 500000;    
+		repeat = 1;
+		compress = 0;
+	}
+	dark_time = 0; 
 
 	if((RasterOrHadamard == 1 && nBasis<nMeas)) {
 		printf("nBasis must be larger tha n nMeas!\n");
